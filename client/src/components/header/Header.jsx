db911056069222be540e7fe9a1ac65f6a2e4787c
chowdhuryvishal9703 <vishalchowdhury333@gmail.com>
2021-06-20 04:09:19 +0530

    Website Completed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,15 +1,16 @@
 import "./header.css";
 
+
 export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">My First</span>
+        <span className="headerTitleSm">React & Node</span>
         <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
-        src="https://cdn.pixabay.com/photo/2021/06/08/11/27/mushrooms-6320451_960_720.jpg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
     </div>
